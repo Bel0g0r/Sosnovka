d0729558d354ef0bf6fa4b583a12894da0102f98
Bel0g0r <46893238+Bel0g0r@users.noreply.github.com>
2021-02-19 01:58:43 +0300

    Рабочая версия приложения VK Apps
    Для сосновка Лофт

---
+++ precache-manifest.13d32574f267841f40c95427d3c09cf3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041d82cea1f10fea9eb1481c44597f6b",
+    "url": "/sushitime/index.html"
+  },
+  {
+    "revision": "3a9dcc812e2099f2672a",
+    "url": "/sushitime/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "e22d3afcc8e92099131b",
+    "url": "/sushitime/static/css/main.3649f681.chunk.css"
+  },
+  {
+    "revision": "3a9dcc812e2099f2672a",
+    "url": "/sushitime/static/js/2.140d579a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sushitime/static/js/2.140d579a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e22d3afcc8e92099131b",
+    "url": "/sushitime/static/js/main.d8abcf04.chunk.js"
+  },
+  {
+    "revision": "1e888aebbe52856a82d4",
+    "url": "/sushitime/static/js/runtime-main.a1cde225.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/sushitime/static/media/persik.4e1ec840.png"
+  }
+]);
